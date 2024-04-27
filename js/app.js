f6161323ddad9fb0c1d348a77118d0f59f996d9b
Jakub Janeček <63880876+KasenX@users.noreply.github.com>
2024-04-27 12:30:22 +0200

    Style dialog

--- js/app.js
+++ js/app.js
@@ -32,12 +32,29 @@ dialogButton.addEventListener('click', event => {
     const inputX = dialog.querySelector('input[name="player-x"]');
     const inputO = dialog.querySelector('input[name="player-o"]');
 
-    if (inputX.value === '' || inputO.value === '') {
-        return; 
+    if (inputX.value.trim() === inputO.value.trim()) {
+        inputX.classList.add('error');
+        setTimeout(() => { inputX.classList.remove('error'); }, 850);
+        inputO.classList.add('error');
+        setTimeout(() => { inputO.classList.remove('error'); }, 850);
+        return;
+    }
+    if (inputX.value.trim() === '') {
+        valid = false;
+        inputX.classList.add('error');
+        setTimeout(() => { inputX.classList.remove('error'); }, 850);
+    }
+    if (inputO.value.trim() === '') {
+        valid = false;
+        inputO.classList.add('error');
+        setTimeout(() => { inputO.classList.remove('error'); }, 850);
+    }
+    if (inputX.value.trim() === '' || inputO.value.trim() === '') {
+        return;
     }
 
-    playerX = inputX.value;
-    playerO = inputO.value;
+    playerX = inputX.value.trim();
+    playerO = inputO.value.trim();
     inputX.value = inputO.value = '';
 
     dialog.close();
